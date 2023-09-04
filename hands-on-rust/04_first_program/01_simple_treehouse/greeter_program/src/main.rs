dc338109bc62b5868cddfda70ae655cb5d9e8ddc
jvela018 <jonathan.velasco@csc.fi>
2023-09-04 22:33:55 +0300

    used cargo fmt for formatting

--- hands-on-rust/04_first_program/01_simple_treehouse/greeter_program/src/main.rs
+++ hands-on-rust/04_first_program/01_simple_treehouse/greeter_program/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     println!("Hello, what's your name?");
     let mut your_name = String::new();
     stdin()
-    .read_line(&mut your_name)
-    .expect("Failed to read line");
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
     println!("Hello, {}", your_name);
 }
