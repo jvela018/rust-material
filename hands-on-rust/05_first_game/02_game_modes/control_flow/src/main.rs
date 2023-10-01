af26acf9a8f809dfc8a9485407c15b5e0a758635
jvela018 <jonathan.velasco@csc.fi>
2023-10-01 15:12:22 +0300

    formated code using cargo fmt

--- hands-on-rust/05_first_game/02_game_modes/control_flow/src/main.rs
+++ hands-on-rust/05_first_game/02_game_modes/control_flow/src/main.rs
@@ -1,62 +1,62 @@
 use bracket_lib::prelude::*;
 
-enum GameMode{
+enum GameMode {
     Menu,
     Playing,
     End,
 }
 
 struct State {
-    mode:GameMode
+    mode: GameMode,
 }
 
-impl State{
-    fn new()->Self{
-        State{
-            mode:GameMode::Menu,
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
         }
     }
-    fn play(&mut self, ctx:&mut BTerm){
+    fn play(&mut self, ctx: &mut BTerm) {
         // TODO: Fill this in stub later
         self.mode = GameMode::End;
     }
-    fn restart(&mut self){
+    fn restart(&mut self) {
         self.mode = GameMode::Playing;
     }
 
-    fn main_menu(&mut self, ctx: &mut BTerm){
+    fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "Welcome to Flappy Dragon");
         ctx.print_centered(8, "(P) Play Game");
         ctx.print_centered(9, "(Q) Quit Game");
-    if let Some(key) = ctx.key{
-    match key{
-        VirtualKeyCode::P => self.restart(),
-        VirtualKeyCode::Q => ctx.quitting = true,
-        _=>{}
-    }
-    }
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 
-    fn dead(&mut self, ctx: &mut BTerm){
+    fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You're dead!");
         ctx.print_centered(8, "(P) Play again");
         ctx.print_centered(9, "(Q) Quit Game");
-    
-        if let Some(key) = ctx.key{
-            match key{
+
+        if let Some(key) = ctx.key {
+            match key {
                 VirtualKeyCode::P => self.restart(),
                 VirtualKeyCode::Q => ctx.quitting = true,
-                _=>{}
+                _ => {}
             }
         }
     }
 }
 
-impl GameState for State{
-    fn tick(&mut self, ctx:&mut BTerm){
-        match self.mode{
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
             GameMode::Menu => self.main_menu(ctx),
             GameMode::End => self.dead(ctx),
             GameMode::Playing => self.play(ctx),
@@ -69,4 +69,5 @@ fn main() -> BError {
         .with_title("Flappy Dragon")
         .build()?;
 
-        main_loop(context, State::new())}
+    main_loop(context, State::new())
+}
